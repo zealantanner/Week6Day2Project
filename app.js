0e5b6f9cb8f054ad467dca25d683c060fbf3dee3
zealantanner <zealantanner@gmail.com>
2021-09-26 11:13:15 -0600

    Basically perfectish?

--- app.js
+++ app.js
@@ -1,3 +1,40 @@
+const closestDivisors = {
+    lower: function(number) {
+        let x = number;
+        let n = Math.floor(Math.sqrt(x));
+        while (n > 0){
+            if (n == 1){
+                console.log(`Congrats ${x} is a prime number!`);
+            }
+            if (x % n == 0){
+                break;
+            }
+            else{
+                n -= 1;
+            }
+        }
+        return n;
+    },
+
+    higher: function(number) {
+        let x = number;
+        let n = Math.floor(Math.sqrt(x));
+        let m = undefined;
+        while (n > 0){
+            if (n == 1){
+                console.log(`Congrats ${x} is a prime number!`);
+            }
+            if (x % n == 0){
+                m = Math.floor(x / n);
+                break;
+            }
+            else{
+                n -= 1;
+            }
+        }
+        return m;
+    }
+};
 
 $('#squareGenerator').on('click', function() {
     for(let i = 0; i < 1; i++){
@@ -7,34 +44,27 @@ $('#squareGenerator').on('click', function() {
     }
 
     let x = totalSquares;
-    let n = Math.floor(Math.sqrt(x));
-
-    while (n > 0){
-        if (n == 1){
-            console.log(`Congrats ${x} is a prime number!`);
-        }
-        if (x % n == 0){
-            m = Math.floor(x / n);
-            break;
-        }
-        else{
-            n -= 1
-        }
-    }
-    console.log(`${n} and ${m} are the closest divisors of ${x}!`);
-
-
+    let n = closestDivisors.lower(totalSquares);
+    let m = closestDivisors.higher(totalSquares);
+    // console.log(`${n} and ${m} are the closest divisors of ${x}!`);
 
     console.log(x + " " + n + " " + m);
     let width = n*100+'px';
     let height = m*100+'px';
     $('#squareContainer').css({
         'width': width,
-        'height': height});
+        'height': height
+    });
+    $('#squareCounter').text("Squares: " + totalSquares);
+    $('#widthCounter').text("Width: " + n);
+    $('#heightCounter').text("Height: " + m);
 });
 
-$('#squareContainer').on('mousedown', '.square', function() {
-    $(this).toggleClass('red blue')
-});
+if($('#squareContainer').on('click', '.square', function(){
+   $(this).toggleClass('red blue');
+}));
 
-
+for(let i = 0; i < 16; i++){
+    let color = (Math.random() < 0.5) ? 'red': 'blue';
+    $('#squareContainer').prepend(`<li class="${color} square"></li>`);
+};
